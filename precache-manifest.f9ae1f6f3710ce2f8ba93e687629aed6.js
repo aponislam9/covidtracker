54d494736e3b94ac102fa9e9ff0e9a649744cb80
aponislam9 <50083322+aponislam9@users.noreply.github.com>
2020-08-20 18:19:10 -0700

    Updates

---
+++ precache-manifest.f9ae1f6f3710ce2f8ba93e687629aed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5db2782d344ccd7dc71b09c6ba30d5",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "4eb850c6adc38194dcf3",
+    "url": "/covidtracker/static/css/main.7e63fd14.chunk.css"
+  },
+  {
+    "revision": "d1eb7b3ead7515fc51e2",
+    "url": "/covidtracker/static/js/2.bec6893a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.bec6893a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb850c6adc38194dcf3",
+    "url": "/covidtracker/static/js/main.562c1ad0.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covidtracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
